344fe6ec898a41d89e0db02b0f4ba81762e02f64
Stefan Ihle <stefanihle@gmx.net>
2022-12-08 10:55:57 +0100

    display messages for each user and connect messages for each user with branches

--- src/components/UserDisplay.tsx
+++ src/components/UserDisplay.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import MessageDisplay from './MessageDisplay';
 import { User } from '../types';
 
 type Props = {
@@ -10,6 +11,11 @@ const UserDisplay = ({ user }: Props) => {
 		<div className='flex gap-x-24 justify-center'>
 			<div className='rounded-sm flex justify-center items-center drop-shadow-md bg-neutral-700 w-48 h-20'>
 				<p className='text-xl text-gray-200 font-bold'>{user.name}</p>
+			</div>
+			<div>
+				{user.messages.map((message, i) => (
+					<MessageDisplay key={i} index={i} message={message} />
+				))}
 			</div>
 		</div>
 	);
